9927f6193eb97104e199df67fa462fc06235843a
Michael Dippery <mdippery@gmail.com>
2010-08-27 01:01:57 -0400

    Add MIT license terms to source code

--- Source/main.m
+++ Source/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  Blackout
-//
-//  Created by Michael Dippery on 8/26/2010.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
