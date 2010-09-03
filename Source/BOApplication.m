e2feb442634abef9e13d4c31eb3fff628f38884e
Michael Dippery <mdippery@gmail.com>
2010-09-03 11:28:21 -0400

    Register hot key for invoking screen saver
    Eventually this will be a configurable option, but for now it's
    hardcoded as Cmd-Shift-Esc.

--- Source/BOApplication.m
+++ Source/BOApplication.m
@@ -21,21 +21,41 @@
  */
 
 #import "BOApplication.h"
+
+#import <Carbon/Carbon.h>
+
 #import "NSEvent+Blackout.h"
 #import "NSImage+Convenience.h"
+
+#define ESC_KEY 53
 
 
 @interface BOApplication ()
 @property (readonly) NSImage *statusMenuImage;
 @property (readonly) NSImage *alternateStatusMenuImage;
+- (void)registerGlobalHotkey;
 - (IBAction)activateScreenSaverOrMenu:(id)sender;
 @end
+
+
+static OSStatus BOHotkeyHandler(EventHandlerCallRef nextHandler, EventRef theEvent, void *userData)
+{
+    // Delay screen saver activation for a half-second -- otherwise
+    // it gets kicked off almost immediately.
+    [(BOApplication *)userData performSelector:@selector(activateScreenSaver:) withObject:(BOApplication *)userData afterDelay:0.5];
+    return noErr;
+}
 
 
 @implementation BOApplication
 
 @dynamic statusMenuImage;
 @dynamic alternateStatusMenuImage;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)note
+{
+    [self registerGlobalHotkey];
+}
 
 - (void)awakeFromNib
 {
@@ -63,6 +83,23 @@
 - (NSImage *)alternateStatusMenuImage
 {
     return [self statusMenuImage];
+}
+
+- (void)registerGlobalHotkey
+{
+    // Source: <http://dbachrach.com/blog/2005/11/program-global-hotkeys-in-cocoa-easily/>
+    
+    EventHotKeyRef hotKeyRef;
+    EventHotKeyID hotKeyID;
+    EventTypeSpec eventType;
+    
+    hotKeyID.signature = 'blo1';
+    hotKeyID.id = 1;
+    eventType.eventClass = kEventClassKeyboard;
+    eventType.eventKind = kEventHotKeyPressed;
+    
+    InstallApplicationEventHandler(BOHotkeyHandler, 1, &eventType, self, NULL);
+    RegisterEventHotKey(ESC_KEY, cmdKey | shiftKey, hotKeyID, GetApplicationEventTarget(), 0, &hotKeyRef);
 }
 
 - (IBAction)activateScreenSaverOrMenu:(id)sender
