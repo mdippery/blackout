e485a42c1494a1bfbfa42ebe0a6c3c730155d5d4
Michael Dippery <michael@monkey-robot.com>
2019-03-01 15:30:22 -0800

    Create preference pane project

---
+++ Preferences/Source/Blackout.h
@@ -0,0 +1,15 @@
+//
+//  Blackout.h
+//  Blackout
+//
+//  Created by Michael Dippery on 3/1/19.
+//  Copyright © 2019 Michael Dippery. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface Blackout : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ Preferences/Source/Blackout.m
@@ -0,0 +1,17 @@
+//
+//  Blackout.m
+//  Blackout
+//
+//  Created by Michael Dippery on 3/1/19.
+//  Copyright © 2019 Michael Dippery. All rights reserved.
+//
+
+#import "Blackout.h"
+
+@implementation Blackout
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
